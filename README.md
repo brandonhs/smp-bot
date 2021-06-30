2fdf70456322c51fafafce6ab920a93d51542126
Brandon S <brandonnms@gmail.com>
2021-06-29 22:02:06 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# smp-bot
+
+Discord bot for the CAMS class of 2024 minecraft server.
---
+++ bot.py
@@ -0,0 +1,64 @@
+import os
+import sys
+
+from urllib.parse import urlparse
+
+import discord
+from discord.ext import commands
+
+from dotenv import load_dotenv
+from periodic import Periodic
+from mcpacket import protocol
+
+from pyngrok import ngrok
+
+import argparse
+
+parser = argparse.ArgumentParser(description='Run the CAMS SMP discord bot', epilog='If no hostname is provided, an ngrok server will be started from localhost.')
+parser.add_argument('--hostname', required = False, type=str, help='The hostname of the server')
+parser.add_argument('--port', required = False, type=int, help='The port of the server', )
+
+args = parser.parse_args()
+
+hostname = args.hostname
+port = args.port
+if not args.hostname:
+    tunnel = ngrok.connect(25565, 'tcp')
+    ngrok_url = tunnel.public_url
+    ngrok_parsed = urlparse(ngrok_url)
+    hostname = ngrok_parsed.hostname
+    port = ngrok_parsed.port
+if not port:
+    port = 25565
+
+print(hostname, port)
+
+protoc = protocol.Protocol(host=hostname, port=port)
+protoc.connect()
+
+bot = commands.Bot('$')
+
+async def update_status():
+    protoc.connect()
+    await bot.change_presence(activity=discord.Game(f'{hostname}:{port} with {protoc.player_count}/{protoc.player_max} online players'))
+
+@bot.command()
+async def smpinfo(ctx: commands.Context):
+    protoc.connect()
+    msg = ''
+    msg += f'{protoc.description.text}\n'
+    msg += f'IP: `{hostname}:{port}`\n\n'
+    msg += f'Online Players: `{protoc.player_count}/{protoc.player_max}`\n'
+    msg += '\n'
+    for player in protoc.player_list:
+        msg += f'**{player.name}** - `{player.id}`\n'
+    await ctx.send(msg)
+
+async def main():
+    p = Periodic(3, update_status)
+    await p.start()
+
+load_dotenv()
+loop = bot.loop
+loop.create_task(main())
+bot.run(os.environ.get('TOKEN'))
